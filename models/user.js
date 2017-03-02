8c52768e3c4b262c6dd9ceccfaebf9adca04a366
triyanarief <triyanarief@icloud.com>
2017-03-02 19:19:17 +0700

    login logout

--- models/user.js
+++ models/user.js
@@ -23,16 +23,16 @@ const UserSchema = mongoose.Schema({
   }
 });
 
-const user = module.exports = mongoose.model('User', UserSchema);
+const User = module.exports = mongoose.model('User', UserSchema);
 
 module.exports.getUserById = function(id, callback){
   User.findById(id, callback);
-};
+}
 
 module.exports.getUserByUsername = function(username, callback){
   const query = {username: username}
-  User.findByOne(query, callback);
-};
+  User.findOne(query, callback);
+}
 
 module.exports.addUser = function(newUser, callback){
   bcrypt.genSalt(10, (err, salt) => {
@@ -44,7 +44,7 @@ module.exports.addUser = function(newUser, callback){
   });
 }
 
-module.exports.comparePassword = function(candidatePassword, user, callback){
+module.exports.comparePassword = function(candidatePassword, hash, callback){
   bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
     if(err) throw err;
     callback(null, isMatch);
